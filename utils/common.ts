34045509c485cd93e0e4a5d7e76fff2747446663
junki <junki646@gmail.com>
2023-03-03 03:34:36 +0900

    Rename : mdRemover postUtils로 이동

--- utils/common.ts
+++ utils/common.ts
@@ -1,5 +1,3 @@
 export const capitalizer = (name: string) => {
   return name.replace(/(^|-)([a-z])/g, (match: string) => match.toUpperCase())
 }
-
-export const mdRemover = (fileName: string) => fileName.replace(/\.md$/, '')
