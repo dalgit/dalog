9947ec0b81617d9ad2b647110fbbb337c0609db3
junki <junki646@gmail.com>
2023-03-07 20:54:04 +0900

    Feat : main header mobile을 위한 반응형 추가

--- components/layout/MainHeader.tsx
+++ components/layout/MainHeader.tsx
@@ -2,65 +2,96 @@ import React from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
 import logo from '/public/assets/icon_logo.png'
+import hamburger from '/public/assets/hamburger.png'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { MAIN_MENUS as menus } from '@/constants/headerMenus'
-
+import { useState } from 'react'
 const MainHeader = () => {
   const router = useRouter()
   const path = router.pathname
+  const [isMenuListOpen, setIsMenuListOpen] = useState<boolean>(false)
 
   return (
-    <Wrapper>
-      <Link href="/">
-        <Image alt="logo" src={logo} height={30} />
-      </Link>
-      <List>
-        {menus.map((menu) => {
-          return (
-            <Item isCurrentUrl={path === menu.path} key={menu.id}>
-              <Link href={menu.path}>{menu.name}</Link>
-            </Item>
-          )
-        })}
-      </List>
-    </Wrapper>
+    <MainHeaderLayout isMenuListOpen={isMenuListOpen}>
+      <MainHeaderInner>
+        <Link href="/">
+          <Image alt="logo" src={logo} height={30} />
+        </Link>
+        <List isMenuListOpen={isMenuListOpen}>
+          {menus.map((menu) => {
+            return (
+              <Item isCurrentUrl={path === menu.path} key={menu.id}>
+                <Link href={menu.path}>{menu.name}</Link>
+              </Item>
+            )
+          })}
+        </List>
+        <HamburgerButton onClick={() => setIsMenuListOpen(!isMenuListOpen)}>
+          <Image alt="logo" src={hamburger} height={30} />
+        </HamburgerButton>
+      </MainHeaderInner>
+    </MainHeaderLayout>
   )
 }
 
 export default MainHeader
 
-const Wrapper = styled.nav`
-  box-sizing: border-box;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  padding: inherit;
+const HamburgerButton = styled.button`
+  background-color: white;
+  border: none;
+  z-index: 10;
+  @media ${({ theme }) => theme.device.laptop} {
+    display: none;
+  }
+`
 
-  border-bottom: 1px solid #f5f5f5;
-
+const MainHeaderInner = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 900px;
 
-  height: ${({ theme }) => theme.layoutHeight.header};
-
-  background-color: rgba(255, 255, 255, 0.5);
+  @media ${({ theme }) => theme.device.mobile} {
+    padding: 0 20px;
+    position: relative;
+  }
 `
-const List = styled.ul`
+
+const MainHeaderLayout = styled.nav<{ isMenuListOpen: boolean }>`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border-bottom: 1.5px solid #f5f5f5;
+  background-color: white;
+  height: ${({ theme }) => theme.layoutHeight.header};
+`
+
+const List = styled.ul<{ isMenuListOpen: boolean }>`
   display: flex;
   align-items: center;
   height: 100%;
+  position: absolute;
+
+  @media ${({ theme }) => theme.device.mobile} {
+    height: 200px;
+    width: 100%;
+    flex-direction: column;
+    left: 0;
+    transition: top 0.3s ease-in-out;
+    top: ${({ isMenuListOpen }) => (isMenuListOpen ? '0' : `-200px`)};
+    background-color: rgba(128, 128, 128, 0.8);
+  }
 `
 
 const Item = styled.div<{ isCurrentUrl: boolean }>`
   display: flex;
   align-items: center;
   height: 100%;
-
   font-size: 26px;
-
   border-bottom: ${({ isCurrentUrl }) => isCurrentUrl && '4px solid #6028e1'};
 
   a {
@@ -69,5 +100,15 @@ const Item = styled.div<{ isCurrentUrl: boolean }>`
 
   &:not(:last-child) {
     margin-right: 60px;
+  }
+
+  @media ${({ theme }) => theme.device.mobile} {
+    border-bottom: none;
+    a {
+      color: white;
+    }
+    &:not(:last-child) {
+      margin-right: 0;
+    }
   }
 `
--- styles/theme.ts
+++ styles/theme.ts
@@ -3,8 +3,21 @@ const layoutHeight = {
   footer: '90px',
 }
 
+const deviceSizes = {
+  mobile: '767px',
+  tablet: { min: '768px', max: '1023px' },
+  laptop: '1024px',
+}
+
+const device = {
+  mobile: `screen and (max-width: ${deviceSizes.mobile})`,
+  tablet: `screen and (min-width:${deviceSizes.tablet.min}) and (max-width: ${deviceSizes.tablet.max})`,
+  laptop: `screen and (min-width: ${deviceSizes.laptop})`,
+}
+
 const theme = {
   layoutHeight,
+  device,
 }
 
 export default theme
