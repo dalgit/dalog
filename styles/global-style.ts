c8eaafe96c4c3263606f23985de13035feffb112
junki <junki646@gmail.com>
2023-03-09 17:03:36 +0900

    design : font추가, scroll변경

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -1,13 +1,34 @@
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
+
 export const GlobalStyle = createGlobalStyle`
     ${reset}
     * {
-       box-sizing: border-box;
-     }
-    a { 
-       cursor: pointer; 
-        text-decoration: none; 
-       color: black;
+      box-sizing: border-box;
+    }
+    a {
+      cursor: pointer;
+      text-decoration: none;
+      color: black;
+    }
+    body {
+      overflow-y: scroll;
+      font-family:'Noto Sans KR', sans-serif;
+    }
+    body::-webkit-scrollbar {
+      width: 5px;
+    }
+    body::-webkit-scrollbar-thumb {
+      height: 20%;
+      background: #b89ef6;
+      border-radius: 1px;
+    }
+
+    body::-webkit-scrollbar-track {
+      background: rgba(33, 122, 244, 0.1);
+    }
+
+    button {
+      cursor: pointer;
     }
 `
