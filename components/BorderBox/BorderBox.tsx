dcbdde5df470d86b0b42a863971fd354ee80cef4
junki <junki646@gmail.com>
2023-05-12 20:11:46 +0900

    feat : add info

---
+++ components/BorderBox/BorderBox.tsx
@@ -0,0 +1,42 @@
+import React, { ReactElement, ReactNode } from 'react'
+import styled, { css } from 'styled-components'
+
+interface BorderBoxprops {
+  children: ReactNode
+  title: string
+}
+
+const BorderBox = ({ children, title }: BorderBoxprops): ReactElement => {
+  return (
+    <BorderBoxLayout>
+      <BorderBoxTitle>{title}</BorderBoxTitle>
+      <ChildrenWrapper>{children}</ChildrenWrapper>
+    </BorderBoxLayout>
+  )
+}
+
+const ChildrenWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
+const BorderBoxLayout = styled.div`
+  border-radius: 5px;
+  padding: 20px;
+  border: 1px solid #dddddd;
+  position: relative;
+  margin: 20px 0;
+`
+
+const BorderBoxTitle = styled.span`
+  position: absolute;
+  top: 0;
+  left: 10%;
+  padding: 5px;
+  background-color: white;
+  transform: translateY(-55%);
+  color: gray;
+`
+
+export default BorderBox
--- pages/info.tsx
+++ pages/info.tsx
@@ -1,7 +1,43 @@
+import Link from 'next/link'
 import React from 'react'
+import styled from 'styled-components'
+import BorderBox from '@/components/BorderBox/BorderBox'
 
-const index = () => {
-  return <div>index</div>
+const InfoPage = () => {
+  return (
+    <InfoPageLayout>
+      <BorderBox title="Me">
+        <Link href="https://github.com/dalgit">
+          <img src="https://img.shields.io/badge/github.com/dalgit-181717?style=for-the-badge&logo=github&logoColor=white" />
+        </Link>
+        <img src="https://img.shields.io/badge/dalgit77@gmail.com-EA4335?style=for-the-badge&logo=gmail&logoColor=white" />
+      </BorderBox>
+      <BorderBox title="Stack">
+        <div />
+        <div>
+          <img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+          <img src="https://img.shields.io/badge/css-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+          <img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+        </div>
+        <div>
+          <img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=black" />
+          <img src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=Next.js&logoColor=white" />
+          <img src="https://img.shields.io/badge/react query-FF4154?style=for-the-badge&logo=react-query&logoColor=white" />
+        </div>
+        <div>
+          <img src="https://img.shields.io/badge/styled components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white" />
+        </div>
+      </BorderBox>
+    </InfoPageLayout>
+  )
 }
 
-export default index
+export default InfoPage
+
+const InfoPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 40px 0;
+`
